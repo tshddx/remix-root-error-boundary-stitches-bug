708bb5c843c21fb540570b770131091c7c2c6488
Thomas Shaddox <wediddoit@gmail.com>
2022-02-04 10:57:17 -0800

    Minimal reproduction of Stitches styles disappearing when re-rendering the html tag

--- app/root.tsx
+++ app/root.tsx
@@ -8,13 +8,19 @@ import {
 } from "remix";
 import type { MetaFunction } from "remix";
 
+import { styled } from '@stitches/react';
+import { useState } from 'react';
+
+const Box = styled('div');
+
 export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
 };
 
 export default function App() {
+  const [index, setIndex] = useState(0);
   return (
-    <html lang="en">
+    <html lang="en" key={index}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
@@ -22,6 +28,18 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <Box
+          css={{ backgroundColor: 'LightPink', padding: '2em', fontSize: 24 }}
+        >
+          <Box>This text and button should be inside a pink box.</Box>
+          <Box
+            as="button"
+            onClick={() => setIndex((i) => i + 1)}
+            css={{ fontSize: 'inherit' }}
+          >
+            Click me to re-render the html tag
+          </Box>
+        </Box>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
